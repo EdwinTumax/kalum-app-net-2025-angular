af75dcc9b7c2d97c1b4b90b435b28992f75b5c78
EdwinTumax <edwintumax@gmail.com>
2025-10-11 11:19:59 -0600

    Integracion de login con Api AuthKalum

--- src/app/layout/menu/menu.ts
+++ src/app/layout/menu/menu.ts
@@ -4,6 +4,8 @@ import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../auth/auth';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-menu',
@@ -18,11 +20,9 @@ import { Router, RouterModule } from '@angular/router';
 })
 export class Menu {
  
-  loggin: boolean = true;
-
   @Output() toggleSidenav = new EventEmitter<void>();
   
-  constructor(private router: Router) {
+  constructor(private router: Router, private authService: AuthService) {
 
   }
 
@@ -30,13 +30,19 @@ export class Menu {
     this.toggleSidenav.emit();
   }
 
-  isLogedIn() {
-    return this.loggin;
-  }
-
   loginLogout() {
-    if(this.isLogedIn()) {
-      this.router.navigate(['/login']);
+    if(this.authService.isAuthenticated()) {   
+      let username = this.authService.user.username;
+      Swal.fire({
+        title: 'Logout',
+        text: `${username}, has cerrado sesión con éxito`,
+        icon: 'success'
+      }).then(result => {
+        if(result.isConfirmed) {
+          this.authService.logout();
+          this.router.navigate(['/login']);
+        }
+      }); 
     } 
   }
 
